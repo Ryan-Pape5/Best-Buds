7b71747705865f20f163d97d28815ea0a4ed22a7
Ryan Pape <ryan.pape5@gmail.com>
2020-06-14 23:45:18 -0400

    initial commit

---
+++ Best Buds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Best Buds
+//
+//  Created by Ryan Pape on 6/14/20.
+//  Copyright © 2020 Ryan Pape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
